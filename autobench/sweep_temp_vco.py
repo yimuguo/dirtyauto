da62b9fa324490c84fea1d6d89d0d26479775dd0
yimuguo <guoyimu@gmail.com>
2016-04-01 18:51:09 -0700

    Including LOGs Implemented SCPI synchronize.

--- autobench/sweep_temp_vco.py
+++ autobench/sweep_temp_vco.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import visa
 from i2c.aa_i2c import AAReadWrite
 import time
@@ -12,79 +13,106 @@ class SweepTempVCO(object):
     def __init__(self):
         self.dut_i2c = AAReadWrite(0, 0x68, True)
         self.power = visa.ResourceManager().open_resource('GPIB0::13::INSTR')
-        self.freq = visa.ResourceManager().open_resource('GPIB0::3::INSTR')
+        self.fcounter = visa.ResourceManager().open_resource('GPIB0::3::INSTR')
         self.dut_i2c.length = 1
         self.power.write("APPL P6V,5,0.3")
         self.power.write("APPL P25V,3.3,0.2")
         self.chamber = visa.ResourceManager().open_resource('GPIB0::1::INSTR')
 
-    def cal_vco(self):
-        self.dut_i2c.aa_write_i2c(40, [0])
-        time.sleep(0.1)
+    def cal_vco(self, tempco=True):
+        if tempco:
+            self.dut_i2c.aa_write_i2c(40, [0])
+            time.sleep(0.1)
         self.dut_i2c.aa_write_i2c(26, [0])
-        time.sleep(0.1)
+        time.sleep(0.05)
         self.dut_i2c.aa_write_i2c(26, [0x80])
-        time.sleep(0.5)
+        time.sleep(0.05)
         vco_band = list(self.dut_i2c.aa_read_i2c(47))
-        time.sleep(0.5)
+        time.sleep(0.05)
         return vco_band[0]
 
     def power_on(self, on=True):
         if on:
-            self.power.write("OUTPUT:STATe ON")
+            self.power.write("OUTPUT:STATe ON;*OPC?")
+            self.power.read()
+            idd = self.power.query('MEASure:CURRent:DC? P6V')
+            # time.sleep(0.01)
+            return float(idd)
         else:
-            self.power.write("OUTPUT:STATe OFF")
+            self.power.write("OUTPUT:STATe OFF;*OPC?")
+            self.power.read()
 
-    def freq_counter(self):
-        self.freq.write(':SENSe:DATA?')
-        time.sleep(0.5)
-        curr_freq = self.freq.read()
-        time.sleep(0.5)
+    def freq(self):
+        curr_freq = self.fcounter.query(':MEAS:FREQ? 100E+006,1')
+        # time.sleep(0.05)
         return float(curr_freq)
 
     def set_temp(self, temp):
         self.chamber.write('TEMP, S' + str(temp))
-        time.sleep(0.5)
+        time.sleep(0.05)
 
     def read_temp(self):
-        self.chamber.write('TEMP?')
-        time.sleep(0.1)
-        read_val = self.chamber.read()
+        # self.chamber.write('TEMP?')
+        # time.sleep(0.05)
+        # read_val = self.chamber.read()
+        # time.sleep(0.05)
+        self.chamber.query("TEMP?")
+        time.sleep(0.05)
+        read_val = self.chamber.query("TEMP?")
+        time.sleep(0.5)
         return float(read_val.split(',')[0])
 
+    def close(self):
+        self.dut_i2c.close()
+        time.sleep(0.1)
+        self.chamber.close()
+        time.sleep(0.1)
+        self.fcounter.close()
+        time.sleep(0.1)
+        self.power.close()
+        time.sleep(0.1)
 
-def main():
+
+def main(start_temp, stop_temp, stepping, serial=0, tol_temp=0.2):
     ak692 = SweepTempVCO()
-    time.sleep(0.01)
-    # ak692.power_on()
-    # band = ak692.dut_i2c.aa_read_i2c(47)
-    # print band
-    # print ak692.cal_vco()
-    # print(ak692.freq_counter()/100 - 10 ** 6)
+    logfile = open("log" + str(serial) + '_' + str(start_temp) + "_" + str(stop_temp) + ".txt", 'w+')
+    time.sleep(0.1)
     ak692.power_on(False)
-    start_temp = -40
-    stop_temp = 90
-    tol_temp = 0.2
-    file = open("log.txt", 'w+')
     # ak692.set_temp(start_temp)
-    for temp in range(start_temp, stop_temp+10, 10):
+    for temp in range(start_temp, stop_temp+stepping, stepping):
         curr_temp = ak692.read_temp()
+        ak692.set_temp(temp)
+        print("RAMP TEMP TO " + str(temp), end='')
         while not((curr_temp < (temp + tol_temp)) and (curr_temp > (temp - tol_temp))):
-            if (curr_temp > (temp + tol_temp)) or (curr_temp < (temp - tol_temp)):
-                ak692.set_temp(temp)
             curr_temp = ak692.read_temp()
-            print(curr_temp)
+            print(".", end='')
+            # print(curr_temp)
         if temp == start_temp:
-            ak692.power_on()
+            print("\nSOAK TIME 2MIN AT STARTING POINT...")
+            time.sleep(120)
+            idd = ak692.power_on()
+            if idd < 0.1:
+                raise EnvironmentError("IDD TOO SMALL PLEASE CHECK SETUP")
             time.sleep(0.05)
-            ak692.cal_vco()
+            band = list(ak692.dut_i2c.aa_read_i2c(47))[0]
+            print("\nLOG: W/O TEMP CAL " + str(band))
+            logfile.write("W/O TEMP CAL " + str(band) + '\n')
+            cal_band = ak692.cal_vco()
+            print("Calibrated with tempco: " + str(cal_band))
             time.sleep(0.05)
-        frequency = ak692.freq_counter()
-        vco_band = ak692.dut_i2c.aa_read_i2c(47)[0]
-        print(str(temp) + '\t' + str(frequency) + '\t' + str(vco_band))
-        file.write(str(temp) + '\t' + str(frequency) + '\t' + str(vco_band) + '\n')
+        vco_band = ak692.cal_vco()
+        frequency = ak692.freq()
+        print('\nLOG: ' + str(temp) + '\t' + str(frequency) + '\t' + str(vco_band))
+        logfile.write(str(temp) + '\t' + str(frequency) + '\t' + str(vco_band) + '\n')
         # if temp == start_temp:
-        ak692.set_temp(stop_temp)
+        # ak692.set_temp(stop_temp)
+    logfile.close()
+    ak692.close()
 
 if __name__ == '__main__':
-    main()
+    unitnum = sys.argv[1]
+    print("STARTING TEMP.......... FROM 90C to -40C")
+    main(90, -40, -10, unitnum)
+    print("\nDone with first round, starting from -40C.....")
+    time.sleep(30)
+    main(-40, 90, 10, unitnum)
