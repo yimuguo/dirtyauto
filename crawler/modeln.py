e5f5adc92b62d672e5043c1f1b8dd3e420d3bb64
Guo <yguo@idt.com>
2017-05-03 22:50:35 -0700

    Daily

--- crawler/modeln.py
+++ crawler/modeln.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 
-page = requests.get("http://direct.modeln.idt.com/")
+page = requests.get("http://corpqlikprod/QvAJAXZfc/AccessPoint.aspx?open=&id=QVS%40corpqv1%7CSales%2FOpportunityMetrics.qvw&client=Ajax")
 soup = BeautifulSoup(page.content, 'html.parser')
 print(soup.prettify)
