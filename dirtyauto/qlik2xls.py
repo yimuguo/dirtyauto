c09271b4e4248442742d6811a5577e8cd116314e
Guo <yguo@idt.com>
2017-07-18 18:41:47 -0700

    regular update

--- dirtyauto/qlik2xls.py
+++ dirtyauto/qlik2xls.py
@@ -4,6 +4,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 import sys
 import configparser
+import time
+import win32com.client
 
 
 try:
@@ -16,7 +18,7 @@ except IndexError:
 def make_cfg():
     _config = configparser.ConfigParser()
     _config['DEFAULT'] = {'Intranet': 'yes',
-                          'dowload_dir': '%USERPROFILE%/Downloads'}
+                          'dowload_dir': '%%USERPROFILE%%/Downloads'}
     if USER_PROFILE is not 'DEFAULT':
         _config[USER_PROFILE]['Intranet'] = 'no'
         usr = input("Input your username:\n")
@@ -30,7 +32,7 @@ def make_cfg():
 
 def click_by_text(txt, _driver, timeout=30):
     wait = WebDriverWait(_driver, timeout)
-    wait.until(expected_conditions.visibility_of_element_located(
+    wait.until(expected_conditions.element_to_be_clickable(
         (By.XPATH, "//*[contains(text(), '%s')]" % txt)))
     element = _driver.find_element_by_xpath(
         "//*[contains(text(), '%s')]" % txt)
@@ -40,9 +42,20 @@ def click_by_text(txt, _driver, timeout=30):
 
 def click_by_xpath(xpath, _driver, timeout=30):
     wait = WebDriverWait(_driver, timeout)
-    wait.until(expected_conditions.visibility_of_element_located(
+    wait.until(expected_conditions.element_to_be_clickable(
         (By.XPATH, xpath)))
     element = _driver.find_element_by_xpath(xpath)
+    time.sleep(5)
+    element.click()
+    return element
+
+
+def click_by_title(title, _driver, timeout=30):
+    wait = WebDriverWait(_driver, timeout)
+    wait.until(expected_conditions.element_to_be_clickable(
+        (By.XPATH, '//*[@title="%s"]' % title)))
+    element = _driver.find_element_by_xpath(
+        '//*[@title="%s"]' % title)
     element.click()
     return element
 
@@ -51,18 +64,20 @@ def main():
     # Read/Create Configuration File Contains Username/Password Info
     config = configparser.ConfigParser()
     config.read('qlik2xls.ini')
-    if config.sections():
-        if USER_PROFILE is not 'DEFAULT':
+    if config[USER_PROFILE]:
+        if not config[USER_PROFILE]['Intranet']:
+            raise ValueError('\"Intranet\" option not found in configuration')
+        if USER_PROFILE != 'DEFAULT':
             if config[USER_PROFILE]:
                 if config[USER_PROFILE]['usr']:
                     if config[USER_PROFILE]['pwd']:
                         pass
                     else:
-                        sys.exit("\"pwd\" IS NOT PRESENT IN CONFIG FILE\n")
+                        raise ValueError("\"pwd\" IS NOT PRESENT IN CONFIG FILE\n")
                 else:
-                    sys.exit("\"usr\" IS NOT PRESENT IN THE CONFIG FILE\n")
+                    raise ValueError("\"usr\" IS NOT PRESENT IN THE CONFIG FILE\n")
             else:
-                sys.exit("USERPROFILE NOT FOUND IN CONFIG FILE\n")
+                raise ValueError("USERPROFILE NOT FOUND IN CONFIG FILE\n")
     else:
         print("qlik2xls NOT FOUND IN DIRECTORY, CREATING DEFAULT CONFIG FILE")
         config = make_cfg()
@@ -72,8 +87,7 @@ def main():
     driver.get(
         "http://corpqlikprod/QvAJAXZfc/opendoc.htm?document=Sales%2FOpportunityMetrics.qvw&host=QVS%40corpqv1")
     driver.maximize_window()
-    if usr_flg is not 'yes':
-        import win32com.client
+    if usr_flg != 'yes':
         shell = win32com.client.Dispatch("WScript.Shell")
         usr = config[USER_PROFILE]['usr']
         pwd = config[USER_PROFILE]['pwd']
@@ -84,10 +98,20 @@ def main():
 
     all_opp = click_by_text('All Opportunities', driver)
 
-    funnel_filter_switch = click_by_xpath(
-        '//*[@id="75"]/div[3]/div/div[1]/div[5]/div/div[3]/div[1]', driver)
+    # funnel_filter_switch = click_by_xpath(
+    #     '//*[@id="75"]/div[3]/div/div[1]/div[5]/div/div[3]/div[1]', driver, timeout=60)
+    # funnel_filter_switch = click_by_xpath(
+    #     '//*[@id="74"]/div[3]/div/div[1]/div[5]/div/div[3]', driver, timeout=60)
+    # time.sleep(3)
+    funnel_filter_switch = click_by_xpath('//*[@title="Pending"]//*[@unselectable="on"]', driver, timeout=100)
 
-    send2xls = click_by_xpath('//*[@id="60"]/div[1]/div[1]/div[1]', driver)
+    # send2xls = click_by_xpath('//*[@id="79"]/div[1]/div[1]/div[1]', driver, timeout=100)
+    import pdb; pdb.set_trace()  # breakpoint fe22be2e //
+    send2xls = click_by_title('Send to Excel', driver, timeout=100)
+
+    driver.get("chrome://downloads")
+    shell = win32com.client.Dispatch("WScript.Shell")
+    shell.Sendkeys("Full Opp Funnel")
 
 
 if __name__ == '__main__':
