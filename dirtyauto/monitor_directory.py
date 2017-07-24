e9bb23c7c26528f1bd381ce890dc4aec62c4ab2a
yimuguo <guoyimu@gmail.com>
2017-07-24 01:29:52 -0700

    start monitor directory

---
+++ dirtyauto/monitor_directory.py
@@ -0,0 +1,23 @@
+import sys
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
+import os
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    event_handler = LoggingEventHandler()
+    observer = Observer()
+    path = os.environ['USERPROFILE']
+    print(path)
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
--- dirtyauto/qlik2xls.py
+++ dirtyauto/qlik2xls.py
@@ -7,15 +7,6 @@ import time
 import win32com.client
 
 
-def make_cfg():
-    _config = configparser.ConfigParser()
-    _config['DEFAULT'] = {'Intranet': 'yes',
-                          'dowload_dir': '%%USERPROFILE%%/Downloads'}
-    with open('qlik2xls.ini', 'w+') as file:
-        _config.write(file)
-    return _config
-
-
 def click_by_text(txt, _driver, timeout=30):
     wait = WebDriverWait(_driver, timeout)
     wait.until(expected_conditions.element_to_be_clickable(
@@ -46,57 +37,88 @@ def click_by_title(title, _driver, timeout=30):
     return element
 
 
-def main():
-    # Read/Create Configuration File Contains Username/Password Info
-    config = configparser.ConfigParser()
-    config.read('qlik2xls.ini')
+class ProcessCFG(object):
 
-    if config['DEFAULT']:
-        if config['DEFAULT']['active_profile']:
-            USER_PROFILE = config['DEFAULT']['active_profile']
-            if not config[USER_PROFILE]['Intranet']:
-                raise ValueError('\"Intranet\" option not found in active profile')
+    intranet = 'yes'
+    usr = None
+    pwd = None
+    profile = 'DEFAULT'
+    download_dir = None
+
+    def __init__(self, file):
+        self._config = configparser.ConfigParser()
+        # self._config.read('qlik2xls.ini')
+        self._config.read(file)
+        self.pre_process()
+        self.process_cfg()
+
+    def pre_process(self):
+        if self._config['DEFAULT']:
+            if self._config['DEFAULT']['active_profile']:
+                self.profile = self._config['DEFAULT']['active_profile']
+                if not self._config[self.profile]['intranet']:
+                    raise ValueError('\"intranet\" option not found in active profile')
+                if self._config[self.profile]['intranet'] != 'yes':
+                    if self._config[self.profile]['usr']:
+                        if self._config[self.profile]['pwd']:
+                            pass
+                        else:
+                            raise ValueError('No password in config file while outside of intranet')
+                    else:
+                        raise ValueError('No username in config file while outside of intranet')
+            else:
+                self.profile = 'DEFAULT'
+                if not self._config[self.profile]['intranet']:
+                    raise ValueError[
+                        '\"intranet\" option not found in active profile']
         else:
-            USER_PROFILE = 'DEFAULT'
-            if not config[USER_PROFILE]['Intranet']:
-                raise ValueError['\"Intranet\" option not found in active profile']
-    else:
-        raise Exception("qlik2xls.ini DEFAULT PROFILE NOT VALID IN DIRECTORY, CREATING DEFAULT CONFIG FILE")
-        config = make_cfg()
+            raise Exception(
+                self.file + " DEFAULT PROFILE NOT VALID IN DIRECTORY, CREATING DEFAULT CONFIG FILE")
+            self._config['DEFAULT'] = {'intranet': 'yes',
+                                       'dowload_dir': 'os_default'}
+            with open(self.file, 'w+') as file:
+                self._config.write(file)
+            self.profile = 'DEFAULT'
 
-    print("Current profile " + USER_PROFILE + '\n')
-    usr_flg = config[USER_PROFILE]['Intranet']
+    def process_cfg(self):
+        self.intranet = self._config[self.profile]['intranet']
+        self.dowload_dir = self._config[self.profile]['download_dir']
+        if self.profile != 'DEFAULT' and self.intranet != 'yes':
+            self.usr = self._config[self.profile]['usr']
+            self.pwd = self._config[self.profile]['pwd']
+
+
+def main():
+    # Read/Create _configuration File Contains Username/Password Info
+    config = ProcessCFG('./qlik2xls.ini')
+
+    print("Current profile " + config.profile + '\n')
 
     driver = webdriver.Chrome()
     driver.get(
         "http://corpqlikprod/QvAJAXZfc/opendoc.htm?document=Sales%2FOpportunityMetrics.qvw&host=QVS%40corpqv1")
     driver.maximize_window()
 
-    if usr_flg != 'yes':
+    if config.intranet != 'yes':
         shell = win32com.client.Dispatch("WScript.Shell")
-        usr = config[USER_PROFILE]['usr']
-        pwd = config[USER_PROFILE]['pwd']
-        shell.Sendkeys(usr)
+        shell.Sendkeys(config.usr)
         shell.Sendkeys("{TAB}")
-        shell.Sendkeys(pwd)
+        shell.Sendkeys(config.pwd)
         shell.Sendkeys("{ENTER}")
 
-    all_opp = click_by_text('All Opportunities', driver, time)
+    import pdb; pdb.set_trace()  # breakpoint 2ed8fdbc //
+    all_opp = click_by_text('All Opportunities', driver, timeout=100)
 
     # funnel_filter_switch = click_by_xpath(
     #     '//*[@id="75"]/div[3]/div/div[1]/div[5]/div/div[3]/div[1]', driver, timeout=60)
     # funnel_filter_switch = click_by_xpath(
     #     '//*[@id="74"]/div[3]/div/div[1]/div[5]/div/div[3]', driver, timeout=60)
     # time.sleep(3)
-    funnel_filter_switch = click_by_xpath('//*[@title="Pending"]//*[@unselectable="on"]', driver, timeout=100)
+    funnel_filter_switch = click_by_xpath(
+        '//*[@title="Pending"]//*[@unselectable="on"]', driver, timeout=120)
 
     # send2xls = click_by_xpath('//*[@id="79"]/div[1]/div[1]/div[1]', driver, timeout=100)
-    import pdb; pdb.set_trace()  # breakpoint fe22be2e //
-    send2xls = click_by_title('Send to Excel', driver, timeout=100)
-
-    driver.get("chrome://downloads")
-    shell = win32com.client.Dispatch("WScript.Shell")
-    shell.Sendkeys("Full Opp Funnel")
+    send2xls = click_by_title('Send to Excel', driver, timeout=120)
 
 
 if __name__ == '__main__':
